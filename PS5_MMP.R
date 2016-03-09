015966d74bfde65e05180bab266c9eefbe0c3564
miguelmariagp <m.pereira@wustl.edu>
2016-03-09 17:48:58 -0600

    Final version
    Just doing so cleansing to provide a better experience for the reader.

--- PS5_MMP.R
+++ PS5_MMP.R
@@ -84,7 +84,6 @@ anes$media_attention<-as.numeric(gsub("\\..*","",anes$prmedia_wkinews))
 #CREATING TRAIN/TEST SETS
 ##########################
 set.seed(1)
-  
 train<-sort(sample(dim(anes)[1],dim(anes)[1]/2,replace=FALSE))
 anes.train<-anes[train,]
 anes.test<-anes[-train,]
@@ -99,20 +98,24 @@ anes.test<-anes[-train,]
 model1 <- lm(ft_dpc ~ ft_hclinton+ft_gwb, anes.train)
 #summary(model1)
 
-
+## Model predicts fairly well
 
 #######################
-#MODEL 2: demographics predicting Obama's feeling thermometer. 
+#MODEL 2: previous vote and media attention
+#######################
+model2<-lm(ft_dpc ~ voted08+VoteObama08+media_attention, anes.train)
+#summary(model2)
+
+## Previous turnout and vote choice do not predict Obama evaluations as well.
+
+#######################
+#MODEL 3: demographics predicting Obama's feeling thermometer. 
 #Owning home used as proxy for income and interacted with black, since the effect of income can be exclusive to non-blacks
 #######################
-model2<-lm(ft_dpc ~ female + educ + own*black, anes.train)
-summary(model2)
+model3<-lm(ft_dpc ~ female + educ + own*black, anes.train)
+#summary(model3)
 
-#######################
-#MODEL 3: previous vote and media attention
-#######################
-model3<-lm(ft_dpc ~ voted08+VoteObama08+media_attention, anes.train)
-summary(model3)
+## This model is the weakest of all three. Still, for a model exclusively based on demographic variables it works decently
 
 
 ########################
@@ -129,60 +132,34 @@ pred3 <- predict(model3, anes.test)
 
 ########################
 ##############
-#######EXERCISE 3
+#######EXERCISES 3 and 4
 ##############
 ########################
-#Training
+#Training outside of the function
 #e's
 #test<-t(aaply(pred.matrix,2,function(x,y) abs(y-x), y=Y.test))
 #a's
 #test2<-t(aaply(test,2,function(x,y) x/abs(y+.01)*100, y=Y.test))
 
 
+#' Fit statistics. 
+#'
+#' Calculates fit statistics a la carte using predictions from regression models. 
+#'
+#' @param Y A vector of true Yi values. 
+#' @param preds A X by 3 matrix where each row corresponds to a predicted Yi value and each
+#' column corresponds to a different regression model.
+#' @param rmse Whether to compute the RMSE statistic or not. 
+#' @param mad Whether to compute the MAD statistic or not. Defaults to T.  
+#' @param rmsle Whether to compute the RMSLE statistic or not. Defaults to T. 
+#' @param mape Whether to compute the MAPE statistic or not. Defaults to T. 
+#' @param meape Whether to compute the MEAPE statistic or not. Defaults to T.
+#'
+#' @return A matrix with fit statistics for all three models added by the user.
+#' @author Miguel Pereira.
 
-fit.stats<-function(Y,preds){
-  #Creates matrix with abs error (each column corresponding to each prediction)
-  e<-t(aaply(preds,2,function(x,y) abs(y-x), y=Y))
-  #Creates matrix with abs percentage error  (each column corresponding to each prediction)
-    #Since the outcome variable can be 0 I added .01 to make the calculation possible
-  a<-t(aaply(e,2,function(x,y) x/abs(y+.01)*100, y=Y))
-  
-  #RMSE
-  RMSE<-apply(e,2,function(x) sum(x^2)/length(x))
-  #MAD
-  MAD<-apply(e,2,median)
-  #RMSLE
-    #Here the function does not allow negative predited values so I transformed negative values into 0s
-    new.preds<-ifelse(preds<0,0,preds)
-  RMSLE<-apply(new.preds,2,function(x,y) sqrt( sum((log(x+1)-log(y+1))^2) / length(x)), y=(Y))
-  #MAPE
-  MAPE<-apply(a,2,function(x) sum(x)/length(x))
-  #MEAPE
-  MEAPE<-apply(a,2,median)
-  return(cbind(RMSE,MAD,RMSLE,MAPE,MEAPE))
-}
+#' @rdname fit.stats.flexible
 
-#Now testing
-pred.matrix<-as.matrix(cbind(Model1=pred1,Model2=pred2,Model3=pred3))
-Y.test<-anes.test$ft_dpc
-fit.stats(Y.test,pred.matrix)
-
-R<-TRUE
-M<-FALSE
-C<-F
-D<-T
-L<-T
-test<-c(R,M,C,D,L)*1
-which(test==1)
-
-x<-as.vector(c(RMSE=TRUE, MAD=TRUE, RMSLE=TRUE, MAPE=TRUE, MEAPE=FALSE))
-x*1
-
-########################
-##############
-#######EXERCISE 4
-##############
-########################
 
 fit.stats.flexible<-function(Y,preds, rmse=TRUE, mad=TRUE, rmsle=TRUE, mape=TRUE, meape=TRUE){
   #Creates matrix with abs error (each column corresponding to each prediction)
@@ -209,32 +186,37 @@ fit.stats.flexible<-function(Y,preds, rmse=TRUE, mad=TRUE, rmsle=TRUE, mape=TRUE
   output<-cbind(RMSE,MAD,RMSLE,MAPE,MEAPE)
   
   #Here I identify which stats the user wants to see
-    #Lower case stats are the arguments of the function
+  #Lower case stats are the arguments of the function
   stats.to.print<-c(rmse,mad,rmsle,mape,meape)*1
   
   #Finally, printing
   ifelse (stats.to.print > 0, return(output[,which(stats.to.print==1)]), 
-         return("If you don't feel like getting some fit statistics, that's fine with me."))
- }
+          return("If you don't feel like getting some fit statistics, that's fine with me."))
+}
 
 
+############
 #Now testing
-#Here the full thing
-fit.stats.flexible(Y.test,pred.matrix)
+############
+pred.matrix<-as.matrix(cbind(Model1=pred1,Model2=pred2,Model3=pred3))
+Y.test<-anes.test$ft_dpc
+
+  #Here the full thing
+  fit.stats.flexible(Y.test,pred.matrix)
 
 
-#Here without the first one
-fit.stats.flexible(Y.test,pred.matrix,rmse=F)
+  #Here without the first one
+  fit.stats.flexible(Y.test,pred.matrix,rmse=F)
 
-#Here only with measure starting with M
-fit.stats.flexible(Y.test,pred.matrix,rmse=F,rmsle=F)
+  #Here only with measure starting with M
+  fit.stats.flexible(Y.test,pred.matrix,rmse=F,rmsle=F)
 
 
-#Here with just with MEAPE
-fit.stats.flexible(Y.test,pred.matrix,rmse=F,rmsle=F, mad=F, mape=F)
-
-#Here what happens when you are too picky
-fit.stats.flexible(Y.test,pred.matrix,rmse=F,rmsle=F, mad=F, mape=F, meape=F)
+  #Here with just with MEAPE
+  fit.stats.flexible(Y.test,pred.matrix,rmse=F,rmsle=F, mad=F, mape=F)
+  
+  #Here what happens when you are too picky
+  fit.stats.flexible(Y.test,pred.matrix,rmse=F,rmsle=F, mad=F, mape=F, meape=F)
 
 
 ########################
@@ -242,9 +224,9 @@ fit.stats.flexible(Y.test,pred.matrix,rmse=F,rmsle=F, mad=F, mape=F, meape=F)
 #######EXERCISE 5
 ##############
 ########################
-fit.stats(Y.test,pred.matrix)
+fit.stats.flexible(Y.test,pred.matrix)
 
-#The first models has the smaller values in all five statistics, followed by the third model.
+#The first models has the smaller values in all five statistics, followed by the second and the third models.
 
 
 ########################
@@ -253,11 +235,30 @@ fit.stats(Y.test,pred.matrix)
 ##############
 ########################
 
-x<-lm(Y.test~rnorm(length(Y.test),50,4))$fitted
-median(abs(x-Y.test))
+#' More fit statistics. 
+#'
+#' Calculates fit statistics using predictions from regression models, and allowing for the inclusion of naive predictions. 
+#'
+#' @param Y A vector of true Yi values. 
+#' @param preds A X by 3 matrix where each row corresponds to a predicted Yi value and each
+#' column corresponds to a different regression model.
+#' @param rmse Whether to compute the RMSE statistic or not. 
+#' @param mad Whether to compute the MAD statistic or not. Defaults to T.  
+#' @param rmsle Whether to compute the RMSLE statistic or not. Defaults to T. 
+#' @param mape Whether to compute the MAPE statistic or not. Defaults to T. 
+#' @param meape Whether to compute the MEAPE statistic or not. Defaults to T.
+#' @param mrae Whether to compute the MRAE statistic or not. Defaults to F.
+#' @param R A vector of naive forecasts
+#'
+#' @return A matrix with fit statistics for all three models added by the user.
+#' @author Miguel Pereira.
+
+#' @rdname fit.stats.extra
+
+  #Note, if the user does not add a vector of naive forecasts, the function randomly generates one.
 
 fit.stats.extra<-function(Y,preds, R=lm(Y~rnorm(length(Y),50,4))$fitted,
-                          rmse=TRUE, mad=TRUE, rmsle=TRUE, mape=TRUE, meape=TRUE, mrae=TRUE){
+                          rmse=TRUE, mad=TRUE, rmsle=TRUE, mape=TRUE, meape=TRUE, mrae=FALSE){
   #Creates matrix with abs error (each column corresponding to each prediction)
   e<-t(aaply(preds,2,function(x,y) abs(y-x), y=Y))
   #Creates matrix with abs percentage error  (each column corresponding to each prediction)
@@ -294,9 +295,8 @@ fit.stats.extra<-function(Y,preds, R=lm(Y~rnorm(length(Y),50,4))$fitted,
 }
 
 #With the default naive estimator
-fit.stats.extra(Y.test,pred.matrix)
+fit.stats.extra(Y.test,pred.matrix, mrae=T)
 
 #With an absurd forecast
 nv<-runif(length(Y.test),0,100)
-fit.stats.extra(Y.test,pred.matrix,R=nv)
-
+fit.stats.extra(Y.test,pred.matrix,R=nv, mrae=T)
